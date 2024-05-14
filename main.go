96f62fb8432bc4d6aa4cd061c4139daa98eb4d05
ge32sub <muhammed.1.akkad@gmail.com>
2024-05-14 13:04:56 +0200

    docker compose

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Book struct {
